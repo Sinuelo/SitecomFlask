f77c2bfa69578f16e7573095946548aa1d9f8d4c
Lucas <98895433+Sinuelo@users.noreply.github.com>
2023-05-13 17:52:33 -0300

    Update routes.py

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -51,7 +51,7 @@ def login():
             flash(f'Falha no Login. E-mail ou senha incorretos', 'alert-danger')
 
     if form_criar_conta.validate_on_submit() and 'botao_submit_criar_conta' in request.form:
-        senha_cript = bcrypt.generate_password_hash(form_criar_conta.senha.data)
+        senha_cript = bcrypt.generate_password_hash(form_criar_conta.senha.data).decode('utf-8')
         usuario = Usuario(username=form_criar_conta.username.data, email=form_criar_conta.email.data, senha=senha_cript)
         database.session.add(usuario)
         database.session.commit()
