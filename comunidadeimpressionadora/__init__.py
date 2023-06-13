038929e821708066ab3a10f2c33249e0dacf6f11
Sinuelo <papahet101@hotmail.com>
2023-06-12 23:24:27 -0300

    "Escondendo a senha"

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = SECRET_KEY
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 print(SECRET_KEY)
 if os.getenv("DATABASE_URL"):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
